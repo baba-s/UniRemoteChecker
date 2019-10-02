b9f320a040a9c20df404a5f5753ff37991d414a4
baba_s <shotababa1206@gmail.com>
2019-10-02 14:16:05 +0900

    first commit

---
+++ Runtime/UniRemoteChecker.cs
@@ -0,0 +1,24 @@
+﻿namespace KoganeUnityLib
+{
+	/// <summary>
+	/// Unity Remote 5 が接続されているかどうか確認するクラス
+	/// </summary>
+	public static class UniRemoteChecker
+	{
+		/// <summary>
+		/// <para>接続されている場合 true を返す</para>
+		/// <para>接続されていても、Unity 再生直後の数フレームは false を返すことがあります</para>
+		/// </summary>
+		public static bool IsRemote
+		{
+			get
+			{
+#if UNITY_EDITOR
+				return UnityEditor.EditorApplication.isRemoteConnected;
+#else
+				return false;
+#endif
+			}
+		}
+	}
+}
